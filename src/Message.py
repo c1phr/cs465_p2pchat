d0259ec3440bd0cffa72d2ba2fe0244cbcb51d6b
Andrew Stebenne <ssonicblue@gmail.com>
2014-09-11 10:42:19 -0700

    added docstring to src/Message.py

--- src/Message.py
+++ src/Message.py
@@ -2,6 +2,17 @@ import time
 
 
 class Message(object):
+    """
+    Messages contain the chat data passed between individual Peers. Through
+    the use of flags, they can also indicate other information about
+    individual peer state. 
+
+    Possible flags for a message:
+     (J) Join Request - request to join the network
+     (M) Message - contains a chat message
+     (N) Name Change - contains a new display name for the sending peer
+     (D) Disconnect - the sending peer is disconnecting from the network
+    """
     def __init__(self, flag, body):
         self.flag = flag
         self.body = body
