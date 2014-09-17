db46ce93040a39ef53c37ba1be82690b9f8ecfc5
Ryan Batchelder <rb688@nau.edu>
2014-09-16 18:24:35 -0700

    Fixes to facilitate some testing

--- src/Send.py
+++ src/Send.py
@@ -6,11 +6,11 @@ tru = True
 
 def send(s):
     myself.socket_con = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #open socket
-    myself.socket_con.connect(("10.18.82.46", myself.connection.Get_Listen_Port())) #connect to particular ip
-    myself.socket_con.send(s)    #send the JSON encoded message
+    myself.socket_con.connect(("10.0.1.4", myself.connection.Get_Listen_Port())) #connect to particular ip
+    myself.socket_con.send(s.encode())    #send the JSON encoded message
 
 while tru:
-    s = raw_input("Message: ")
+    s = input("Message: ")
     if s == "e":
         myself.socket_con.close()          #close the socket
         tru = False
