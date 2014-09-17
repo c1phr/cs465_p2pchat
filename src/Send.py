ea8ba0410f16b2ca916dd255fd6acbedd7be9ab7
Salbottig <salbottig@gmail.com>
2014-09-16 18:41:47 -0700

    Made the send work for everyone's ip

--- src/Send.py
+++ src/Send.py
@@ -6,7 +6,7 @@ tru = True
 
 def send(s):
     myself.socket_con = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #open socket
-    myself.socket_con.connect(("10.0.1.4", myself.connection.Get_Listen_Port())) #connect to particular ip
+    myself.socket_con.connect((myself.connection.Get_IP(), myself.connection.Get_Listen_Port())) #connect to particular ip
     myself.socket_con.send(s.encode())    #send the JSON encoded message
 
 while tru:
